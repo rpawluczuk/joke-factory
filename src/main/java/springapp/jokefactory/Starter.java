98a7f8b190f55dd86a94cb11a95417132fda5c5c
Radosław Pawluczuk <radoslaw.pawluczuk@sollers.eu>
2020-01-17 16:50:44 +0100

    Loading list of joke samples to the view

---
+++ src/main/java/springapp/jokefactory/Starter.java
@@ -0,0 +1,30 @@
+package springapp.jokefactory;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+import springapp.jokefactory.domain.repository.JokeRepository;
+import springapp.jokefactory.domain.repository.StructureRepository;
+import springapp.jokefactory.services.StructureService;
+
+@Component
+@Scope("singleton")
+public class Starter implements CommandLineRunner {
+
+    @Autowired
+    JokeRepository jokeRepository;
+
+    @Autowired
+    StructureRepository structureRepository;
+
+    @Autowired
+    StructureService structureService;
+
+
+    @Override
+    public void run(String... args) throws Exception {
+        jokeRepository.createJoke("joke1", "content1");
+        jokeRepository.createJoke("joke2", "content2");
+    }
+}
