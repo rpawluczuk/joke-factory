45c48baffeaf3a712f7964c18819147b045ccc3c
Radosław Pawluczuk <radoslaw.pawluczuk@sollers.eu>
2020-02-04 01:02:06 +0100

    new entities: User, Role login using database

--- src/main/java/springapp/jokefactory/Starter.java
+++ src/main/java/springapp/jokefactory/Starter.java
@@ -5,8 +5,12 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 import springapp.jokefactory.domain.Joke;
+import springapp.jokefactory.domain.Role;
+import springapp.jokefactory.domain.User;
 import springapp.jokefactory.domain.repository.JokeRepository;
+import springapp.jokefactory.domain.repository.RoleRepository;
 import springapp.jokefactory.domain.repository.StructureRepository;
+import springapp.jokefactory.domain.repository.UserRepository;
 import springapp.jokefactory.services.StructureService;
 
 import javax.transaction.Transactional;
@@ -24,6 +28,11 @@ public class Starter implements CommandLineRunner {
     @Autowired
     StructureService structureService;
 
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    RoleRepository roleRepository;
 
     @Override
     @Transactional
@@ -33,5 +42,18 @@ public class Starter implements CommandLineRunner {
 
         structureRepository.createStrcture("Structure1", "Description1");
         structureRepository.createStrcture("Structure2", "Description2");
+
+        User user1 = new User("user1", "{noop}user1");
+        userRepository.persistUser(user1);
+        User user2 = new User("user2", "{noop}user2");
+        userRepository.persistUser(user2);
+
+        Role user1RoleUSER = new Role("user1", "USER");
+        Role user2RoleUSER = new Role("user2", "USER");
+        Role user2RoleADMIN = new Role("user2", "ADMIN");
+
+        roleRepository.persistRole(user1RoleUSER);
+        roleRepository.persistRole(user2RoleUSER);
+        roleRepository.persistRole(user2RoleADMIN);
     }
 }
--- src/main/java/springapp/jokefactory/config/SecurityConfig.java
+++ src/main/java/springapp/jokefactory/config/SecurityConfig.java
@@ -6,27 +6,37 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    DataSource dataSource;
 
     @Override
     public void configure(HttpSecurity security) throws Exception {
 
         security.authorizeRequests()
                 .antMatchers("h2-console/**").permitAll()
-                .antMatchers("/jokes").hasAnyRole("USER", "ADMIN")
-                .antMatchers("/joke").hasAnyRole("ADMIN")
+                .antMatchers("/jokes").hasAnyAuthority("USER", "ADMIN")
+                .antMatchers("/joke").hasAnyAuthority("ADMIN")
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
                 .defaultSuccessUrl("/jokes");
+
+        // settings only for development purposes
+        // For production should be commented
+        security.csrf().disable();
+        security.headers().frameOptions().disable();
     }
 
     @Autowired
     public void securityUsers(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
-        authenticationManagerBuilder.inMemoryAuthentication()
-                .withUser("user1").password("{noop}user1").roles("USER")
-                .and()
-                .withUser("user2").password("{noop}user2").roles("ADMIN");
+        authenticationManagerBuilder.jdbcAuthentication()
+                .dataSource(dataSource)
+                .usersByUsernameQuery("SELECT username, password, enabled FROM USER WHERE username = ?")
+                .authoritiesByUsernameQuery("SELECT username, role FROM ROLE WHERE username = ?");
     }
 }
