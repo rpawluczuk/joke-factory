f6c5c578091ddcad1f40e6c682288153fe8976f3
Radosław Pawluczuk <radoslaw.pawluczuk@sollers.eu>
2020-03-26 15:40:52 +0100

    delete tests

--- src/test/java/springapp/jokefactory/JokeFactoryApplicationTests.java
+++ src/test/java/springapp/jokefactory/JokeFactoryApplicationTests.java
@@ -1,52 +1,14 @@
 package springapp.jokefactory;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import springapp.jokefactory.domain.Joke;
-import springapp.jokefactory.domain.repository.JokeRepository;
-
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
 
 
 @SpringBootTest
 class JokeFactoryApplicationTests {
 
-    @Autowired
-    JokeRepository jokeRepository;
-
     @Test
     void contextLoads() {
     }
 
-    @Test
-    public void testCreate(){
-        Joke joke = new Joke();
-        joke.setTitle("żart o gejach");
-        joke.setContent("ten tego");
-        joke.setAuthor("Adam Mickiewicz");
-        jokeRepository.save(joke);
-    }
-
-    @Test
-    public void testRead(){
-        Joke joke = jokeRepository.findById(1).get();
-        assertNotNull(joke);
-        assertEquals("żart o gejach", joke.getTitle());
-    }
-
-    @Test
-    public void testUpdate(){
-        Joke joke = jokeRepository.findById(1).get();
-        joke.setTitle("Najkrótszy żart o gejach");
-        jokeRepository.save(joke);
-    }
-
-    @Test
-    public void testDelete(){
-        if (jokeRepository.existsById(1)){
-            jokeRepository.deleteById(1);
-        }
-    }
 }
