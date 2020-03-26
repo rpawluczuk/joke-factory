3a00b617348d6ec01e8999f6f351f9507bd02bab
Radosław Pawluczuk <radoslaw.pawluczuk@sollers.eu>
2020-03-26 15:45:46 +0100

    delete tests

--- src/test/java/springapp/jokefactory/JokeFactoryApplicationTests.java
+++ src/test/java/springapp/jokefactory/JokeFactoryApplicationTests.java
@@ -1,14 +1,13 @@
 package springapp.jokefactory;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 
 @SpringBootTest
 class JokeFactoryApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+//    @Test
+//    void contextLoads() {
+//    }
 
 }
