f393d51c8ca455f9cc023ec849a7a94e8c65111b
Radosław Pawluczuk <radoslaw.pawluczuk@sollers.eu>
2020-03-29 17:05:50 +0200

    Springbootinitializer extended

--- src/main/java/springapp/jokefactory/JokeFactoryApplication.java
+++ src/main/java/springapp/jokefactory/JokeFactoryApplication.java
@@ -2,12 +2,18 @@ package springapp.jokefactory;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class JokeFactoryApplication {
+public class JokeFactoryApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(JokeFactoryApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(JokeFactoryApplication.class, args);
     }
-
 }
